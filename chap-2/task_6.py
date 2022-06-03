b15ac4d57a9a7b09e12e6f023ed519eb28e3b951
Saad Guessous <guessous_saad@hotmail.com>
2022-06-03 22:20:49 +0300

    Demonstrate use of asyncio tasks

---
+++ chap-2/task_6.py
@@ -0,0 +1,17 @@
+import asyncio
+from util import delay
+
+
+async def main():
+    task = asyncio.create_task(delay(10))
+
+    try:
+        result = await asyncio.wait_for(asyncio.shield(task), 5)
+        print(result)
+    except asyncio.exceptions.TimeoutError:
+        print("Task took longer than five seconds, it will finnish soon!")
+        result = await task
+        print(result)
+
+
+asyncio.run(main())
