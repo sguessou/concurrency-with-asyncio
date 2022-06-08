cc162a5759197f09ee47c5ac31d5e598168fac60
Saad Guessous <guessous_saad@hotmail.com>
2022-06-08 20:04:51 +0300

    Add graceful shutdown

--- chap-3/socket_asyncio.py
+++ chap-3/socket_asyncio.py
@@ -1,7 +1,9 @@
 import asyncio
 import logging
+import signal
 import socket
 from asyncio import AbstractEventLoop
+from typing import List
 
 
 async def echo(connection: socket,
@@ -16,13 +18,30 @@ async def echo(connection: socket,
     finally:
         connection.close()
 
+echo_tasks = []
+
 async def listen_for_connection(server_socket: socket,
                                 loop: AbstractEventLoop):
     while True:
         connection, address = await loop.sock_accept(server_socket)
         connection.setblocking(False)
         print(f"Got a connection from {address}")
-        asyncio.create_task(echo(connection, loop))
+        echo_task = asyncio.create_task(echo(connection, loop))
+        echo_tasks.append(echo_task)
+
+class GracefulExit(SystemExit):
+    pass
+
+def shutdown():
+    raise GracefulExit()
+
+async def close_echo_tasks(echo_tasks: List[asyncio.Task]):
+    waiters = [asyncio.wait_for(task, 2) for task in echo_tasks]
+    for task in waiters:
+        try:
+            await task
+        except asyncio.exceptions.TimeoutError:
+            pass
 
 async def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,6 +52,16 @@ async def main():
     server_socket.setblocking(False)
     server_socket.listen()
 
+    for signame in {'SIGINT', 'SIGTERM'}:
+        loop.add_signal_handler(getattr(signal, signame), shutdown)
+
     await listen_for_connection(server_socket, asyncio.get_event_loop())
 
-asyncio.run(main())
+loop = asyncio.new_event_loop()
+
+try:
+    loop.run_until_complete(main())
+except GracefulExit:
+    loop.run_until_complete(close_echo_tasks(echo_tasks))
+finally:
+    loop.close()
