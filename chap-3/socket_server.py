0a430677e5a1397400575199b811f31e6eb201bc
Saad Guessous <guessous_saad@hotmail.com>
2022-06-08 16:39:36 +0300

    Implement socket server with asyncio

--- chap-3/socket_server.py
+++ chap-3/socket_server.py
@@ -1,4 +1,9 @@
 import socket
+import selectors
+from selectors import SelectorKey
+from typing import List, Tuple
+
+selector = selectors.DefaultSelector()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -8,33 +13,24 @@ server_socket.bind(server_address)
 server_socket.listen()
 server_socket.setblocking(False)
 
-connections = []
+selector.register(server_socket, selectors.EVENT_READ)
 
-try:
-    while True:
-        try:
-            connection, client_address = server_socket.accept()
+
+while True:
+    events: List[Tuple[SelectorKey, int]] = selector.select(timeout=1)
+
+    if len(events) == 0:
+        print('No events, waiting a bit more!')
+
+    for event, _ in events:
+        event_socket = event.fileobj
+
+        if event_socket == server_socket:
+            connection, address = server_socket.accept()
             connection.setblocking(False)
-            print(f'I got a connection from {client_address}!')
-            connections.append(connection)
-        except BlockingIOError:
-            pass
-
-        for connection in connections:
-            try:
-                buffer = b''
-
-                while buffer[-2:] != b'\r\n':
-                    data = connection.recv(2)
-                    if not data:
-                        break
-                    else:
-                        print(f'I got data: {data}!')
-                        buffer += data
-
-                print(f"All the data is: {buffer}")
-                connection.send(buffer)
-            except BlockingIOError:
-                pass
-finally:
-    server_socket.close()
+            print(f'I got a connection from {address}!')
+            selector.register(connection, selectors.EVENT_READ)
+        else:
+            data = event_socket.recv(1024)
+            print(f"I got some data: {data}")
+            event_socket.send(data)
