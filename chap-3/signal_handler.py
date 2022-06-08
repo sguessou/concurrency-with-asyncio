1bd338b871baa3b4342f7918b631487052a5c363
Saad Guessous <guessous_saad@hotmail.com>
2022-06-08 16:40:12 +0300

    Demo signal handling with asyncio

---
+++ chap-3/signal_handler.py
@@ -0,0 +1,20 @@
+import asyncio
+import signal
+from asyncio import AbstractEventLoop
+from typing import Set
+
+from util.delay_functions import delay
+
+
+def cancel_tasks():
+    print('Got a SIGINT')
+    tasks: Set[asyncio.Task] = asyncio.all_tasks()
+    print(f'Cancelling {len(tasks)} task(s).')
+    [task.cancel() for task in tasks]
+
+async def main():
+    loop: AbstractEventLoop = asyncio.get_running_loop()
+    loop.add_signal_handler(signal.SIGINT, cancel_tasks)
+    await delay(10)
+
+asyncio.run(main())
