00e24389504089fd7291cda9bb509dee957cc404
Saad Guessous <guessous_saad@hotmail.com>
2022-06-21 22:19:52 +0300

    Process results with wait as they complete

---
+++ chap-4/wait_5.py
@@ -0,0 +1,32 @@
+import asyncio
+import importlib
+import aiohttp
+from aiohttp import ClientSession
+import logging
+from util import async_timed
+
+chap_4 = importlib.import_module("chap-4")
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        url = "https://www.example.com"
+        fetchers = [
+            asyncio.create_task(chap_4.fetch_status(session, url)),
+            asyncio.create_task(chap_4.fetch_status(session, url, delay=2)),
+            asyncio.create_task(chap_4.fetch_status(session, url, delay=2)),
+        ]
+
+        done, pending = await asyncio.wait(
+            fetchers, return_when=asyncio.FIRST_COMPLETED
+        )
+
+        print(f"Done task count: {len(done)}")
+        print(f"Pending task count: {len(pending)}")
+
+        for done_task in done:
+            print(await done_task)
+
+
+asyncio.run(main())
